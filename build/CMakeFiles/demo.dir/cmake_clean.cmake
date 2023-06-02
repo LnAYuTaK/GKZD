764df10e02c43c4957f60e7e7ae305f900f21fc7
LnAuTaK <807874484@qq.com>
2023-06-02 17:49:01 +0800

    20236.2

---
+++ build/CMakeFiles/demo.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo.dir/src/app/App.cpp.o"
+  "CMakeFiles/demo.dir/src/log/CLOG.cpp.o"
+  "CMakeFiles/demo.dir/src/main.cpp.o"
+  "CMakeFiles/demo.dir/src/thirdparty/CSerialPort/SerialPort.cpp.o"
+  "CMakeFiles/demo.dir/src/thirdparty/CSerialPort/SerialPortBase.cpp.o"
+  "CMakeFiles/demo.dir/src/thirdparty/CSerialPort/SerialPortInfo.cpp.o"
+  "CMakeFiles/demo.dir/src/thirdparty/CSerialPort/SerialPortInfoBase.cpp.o"
+  "CMakeFiles/demo.dir/src/thirdparty/CSerialPort/SerialPortInfoUnixBase.cpp.o"
+  "CMakeFiles/demo.dir/src/thirdparty/CSerialPort/SerialPortInfoWinBase.cpp.o"
+  "CMakeFiles/demo.dir/src/thirdparty/CSerialPort/SerialPortUnixBase.cpp.o"
+  "CMakeFiles/demo.dir/src/thirdparty/CSerialPort/SerialPortWinBase.cpp.o"
+  "../bin/demo.pdb"
+  "../bin/demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
